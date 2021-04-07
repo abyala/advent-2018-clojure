f9fa0295737f161c19ec229bbb80c2f6fe37afe3
Andrew Byala <abyala@yahoo.com>
2021-04-07 17:06:27 -0500

    Day 8

--- src/advent_2018_clojure/day08.clj
+++ src/advent_2018_clojure/day08.clj
@@ -1,40 +1,41 @@
 (ns advent-2018-clojure.day08
   (:require [clojure.string :as str]))
 
-(def header-length 2)
 (declare parse-children)
 
-(defn node-value [children metadata]
-  (if (empty? children)
-    (apply + metadata)
-    (->> metadata
-         (map #(get-in children [(dec %) :value] 0))
-         (apply +))))
+(def header-length 2)
 
 (defn parse-node [nums]
   (let [[n-children meta-length & others] nums
-        children (parse-children n-children (drop header-length nums))
-        children-length (->> (map :length children) (apply +))
+        [children-length children] (parse-children n-children (drop header-length nums))
         metadata (->> others (drop children-length) (take meta-length))
         total-length (+ header-length children-length meta-length)]
-    {:children     children
-     :metadata     metadata
-     :metadata-sum (+ (apply + metadata)
-                      (apply + (map :metadata-sum children)))
-     :length       total-length
-     :value        (node-value children metadata)}))
+    [total-length {:children children :metadata metadata}]))
 
 (defn parse-children [num-children nums]
   (loop [children [], offset 0]
     (if (= num-children (count children))
-      children
-      (let [child (parse-node (drop offset nums))]
+      [offset children]
+      (let [[len child] (parse-node (drop offset nums))]
         (recur (conj children child)
-               (+ offset (:length child)))))))
+               (+ offset len))))))
 
 (defn parse-tree [input]
-  (parse-node (map #(Integer/parseInt %)
-                   (str/split input #" "))))
+  (->> (str/split input #" ")
+       (map #(Integer/parseInt %))
+       (parse-node)
+       second))
 
-(defn part1 [input] (-> input parse-tree :metadata-sum))
-(defn part2 [input] (-> input parse-tree :value))
+(defn deep-sum-of-metadata [{:keys [children metadata]}]
+  (+ (apply + metadata)
+     (apply + (map deep-sum-of-metadata children))))
+
+(defn deep-node-value [{:keys [children metadata]}]
+  (if (empty? children)
+    (apply + metadata)
+    (->> metadata
+         (map #(-> (get children (dec %) {}) deep-node-value))
+         (apply +))))
+
+(defn part1 [input] (-> input parse-tree deep-sum-of-metadata))
+(defn part2 [input] (-> input parse-tree deep-node-value))
